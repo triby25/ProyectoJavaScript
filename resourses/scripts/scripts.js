4244dedda02c557089e5e10d6fe3751a6e1d4aa5
unknown <triby25@gmail.com>
2017-12-03 23:46:33 -0400

    agregando localStorage y borrado de estudiantes

--- resourses/scripts/scripts.js
+++ resourses/scripts/scripts.js
@@ -9,83 +9,32 @@ Estudiante=function(){
 	self.identificacion="";
 	self.telefono="";
 	self.email="";
-}
+}		
 
-function addRow(){
-	var estu=new Estudiante();
-	estu.id=document.getElementById("id").value;
-	if(estu.id==''){alert('Debe Ingresar un Id válido'); return;}
-	estu.nombre =document.getElementById("nombre").value;
-	estu.matricula=document.getElementById("matricula").value;
-	estu.identificacion=document.getElementById("identificacion").value;
-	estu.telefono=document.getElementById("telefono").value;
-	estu.email=document.getElementById("email").value;
-					
-	addEstudiante(estu);
-};
-			
-
-function addEstudiante(estu){
-	var table=document.getElementById("estudiantes");
-	var tr=document.createElement("tr");
-				
-	var tdId=document.createElement("td");	
-	var txtId=document.createTextNode(estu.id);
-				
-	var tdNombre=document.createElement("td");	
-	var txtNombre=document.createTextNode(estu.nombre);
-				
-	var tdMatricula=document.createElement("td");
-	var txtMatricula=document.createTextNode(estu.matricula);
-				
-	var tdIdentifcacion=document.createElement("td");
-	var txtIdentifcacion=document.createTextNode(estu.identificacion);
-						
-	var tdTelefono=document.createElement("td");
-	var txtTelefono=document.createTextNode(estu.telefono);
-				
-	var tdEmail=document.createElement("td");	
-	var txtEmail=document.createTextNode(estu.email);
+function addEstudiante(estu){	
+	var rowEstudiante ='<tr id="row'+estu.id+'">'+
+	'<td><input type="checkbox" id="'+estu.id+'"></td>'+
+	'<td>'+estu.id+'</td>'+
+	'<td>'+estu.nombre+'</td>'+
+	'<td>'+estu.matricula+'</td>'+
+	'<td>'+estu.identificacion+'</td>'+
+	'<td>'+estu.telefono+'</td>'+
+	'<td>'+estu.email+'</td>'+
+	'<td><button type="button" onclick=borraFila('+estu.id+') class="btn btn-danger" >X</button></td>'+
+	'</tr>'
 	
-	var tdBoton=document.createElement("td");
-	/*var btn = document.createElement('input');
-	btn.type = "button";
-	btn.className = "btn btn-danger" ;
-	btn.value = "X";
-	btn.onclick = alert();*/
-	tdBoton.innerHTML = '<button type="submit" onclick="borraFila('+estu.id+','+table.getElementsByTagName("tr").length+')" class="btn btn-danger" >X</button>';			
+	$("#estudiantes").append(rowEstudiante);
 	
-	tdId.appendChild(txtId);
-	tdNombre.appendChild(txtNombre);
-	tdMatricula.appendChild(txtMatricula);
-	tdIdentifcacion.appendChild(txtIdentifcacion);
-	tdTelefono.appendChild(txtTelefono);
-	tdEmail.appendChild(txtEmail);
-	//tdBoton.appendChild(btn);
-	
-	tr.appendChild(tdId);
-	tr.appendChild(tdNombre);
-	tr.appendChild(tdMatricula);
-	tr.appendChild(tdIdentifcacion);
-	tr.appendChild(tdTelefono);
-	tr.appendChild(tdEmail);
-	tr.appendChild(tdBoton);
-				
-	table.appendChild(tr);
-				
 	listaEstudiantes.push(estu);
-				
+
 	limpiarInputs("divformulario");
 				
-	//console.log(listaEstudiantes);
 	grabarEstuduantes();
 };
 				
 function limpiarInputs(contenedor){
 	var listaInputs = document.getElementById(contenedor).querySelectorAll("input[type='text'],input[type='password'],input[type='email']");
-				
-	//document.querySelectorAll("input[type='text'],input[type='password'],input[type='email']");
-				
+		
 	for (var i = 0; i < listaInputs.length; i++) {
 		listaInputs[i].value="";
 	}
@@ -96,50 +45,17 @@ function grabarEstuduantes(){
 	localStorage.setItem('estudiantes', JSON.stringify(listaEstudiantes));
 };
 			
-function cargarEstuduantes(){
-	listaEstudiantes=[];
+function borraFila(idEstudiante){
 	var estudiantesGuardados = localStorage.getItem('estudiantes');
 	localStorage.removeItem("estudiantes");
-				
-	var lista=JSON.parse(estudiantesGuardados);
-				
-	if(lista==null){
-		alert("No se encontraron estudiantes guardados");
-		return;
-	}
-				
-	var tabla = document.getElementById('estudiantes');
-	while(tabla.childNodes.length>2){
-		tabla.removeChild(tabla.lastChild);
-	}
-
-				
-	for (var i = 0; i < lista.length; i++) {
-		addEstudiante(lista[i])
-	}
 	
-	//tabla.setAttribute('class', 'table table-hover');
-};
-			
-function borrarTodosEstuduantes(){
-	localStorage.removeItem("estudiantes");
-	
-	var tabla = document.getElementById('estudiantes');
-	while(tabla.childNodes.length>2){
-		tabla.removeChild(tabla.lastChild);
-	}
-};
-
-function borraFila(idEstudiante,row){
-	var estudiantesGuardados = localStorage.getItem('estudiantes');
-	localStorage.removeItem("estudiantes");
-				
 	var lista=JSON.parse(estudiantesGuardados);
 	for (var i = 0; i < lista.length; i++) {
 		if(lista[i].id==idEstudiante){
 			lista.splice(i, 1);
 			//delete lista[i];
-			document.getElementById("estudiantes").deleteRow(row);
+			var rowDelete=document.getElementById("row"+idEstudiante).rowIndex-1;
+			document.getElementById("estudiantes").deleteRow(rowDelete);
 			listaEstudiantes=lista;
 			grabarEstuduantes();
 			break;
@@ -147,32 +63,53 @@ function borraFila(idEstudiante,row){
 	}
 };	
 
+function validaIdExistente(idEstudiante){
+	for (var i = 0; i < listaEstudiantes.length; i++) {
+		if(listaEstudiantes[i].id==idEstudiante){
+			return true;
+		}
+	}
+	return false;
+};
+
+$(document).ready(function(){
+	
+	var lista = localStorage.getItem('estudiantes');
+	if(lista!=null){
+		var estudiantesGuardados= JSON.parse(lista);
+		$.each(estudiantesGuardados,function(i,est){
+			addEstudiante(est);
+		});
+	}
+	
+	$("#agregar-Estudiante").click(function(){
+		var estu=new Estudiante();
+		estu.id=$("#id").val();
+		if(estu.id==''){alert('Debe Ingresar un Id válido'); return;}
+		if (isNaN(estu.id)) { alert('Debe Ingresar un Id numérico'); return;}
+		if(validaIdExistente(estu.id)){ alert('El id indicado ya existe'); return;}
+		
+		estu.nombre =$("#nombre").val();
+		estu.matricula=$("#matricula").val();
+		estu.identificacion=$("#identificacion").val();
+		estu.telefono=$("#telefono").val(); 
+		estu.email=$("#email").val(); 
+						
+		addEstudiante(estu);
+	})
+	
+	$("#borrarTodosEstuduantes").click(function(){
+		localStorage.removeItem("estudiantes");
+		lista=[];
+		$("#estudiantes").html("");
+	})	
+	
+
+	$("#borrar-seleccionados").click(function(){
+		$("input:checkbox:checked").each(function() {
+			borraFila($(this).attr("id"));
+		});
+	})
+})
 
 
-
-
-			
-function imprimir(){
-	var id=document.getElementById("id").value;
-	console.log(id);
-	var nombre=document.getElementById("nombre").value;
-	console.log(nombre);
-	var matricula=document.getElementById("matricula").value;
-	console.log(matricula);
-	var identificacion=document.getElementById("identificacion").value;
-	console.log(identificacion);
-	var telefono=document.getElementById("telefono").value;
-	console.log(telefono);
-	var email=document.getElementById("email").value;
-	console.log(email);
-					
-					
-	var estu=new Estudiante();
-	estu.id=id;
-	estu.nombre =nombre;
-	estu.matricula=matricula;
-	estu.identificacion=identificacion;
-	estu.telefono=telefono;
-	estu.email=email;
-	console.log(estu);					
-};		
